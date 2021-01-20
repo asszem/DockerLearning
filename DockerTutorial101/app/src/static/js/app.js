487a240da0627d57e46ff13926dcfeaa270eb4e6
Andras Olah <andras.antal.olah@hu.ibm.com>
2021-01-20 19:10:18 +0100

    Add docker-compose.yml from Getting Started tutorial

--- DockerTutorial101/app/src/static/js/app.js
+++ DockerTutorial101/app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item updated in host machine App folder'}
+                        {submitting ? 'Adding...' : 'Add Item updated in host machine App folder Glu Glu Glu Blö'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
